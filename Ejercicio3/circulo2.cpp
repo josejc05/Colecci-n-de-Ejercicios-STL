10a37be8e4282d23df5fff09d0af8fa22aa9b146
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-15 17:26:31 +0100

    main2 y circulo2

---
+++ Ejercicio3/circulo2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+namespace Geometria {
+    class Circulo {
+    private:
+        double radio;
+    public:
+        Circulo(double r) : radio(r) {}
+        double calcularArea() {
+            return 3.14 * radio * radio;
+        }
+        double calcularPerimetro() {
+            return 2 * 3.14 * radio;
+        }
+    };
+}
---
+++ Ejercicio3/main2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "circulo2.cpp"
+#include "triangulo.cpp"
+#include "rectangulo.cpp"
+#include "cuadrado.cpp"
+#include "trapecio.cpp"
+
+int main() {
+    Geometria::Circulo circulo(5);
+    std::cout << "Área del círculo: " << circulo.calcularArea() << std::endl;
+    std::cout << "Perímetro del círculo: " << circulo.calcularPerimetro() << std::endl;
+
+    Geometria::Triangulo triangulo(4, 3);
+    std::cout << "Área del triángulo: " << triangulo.calcularArea() << std::endl;
+    std::cout << "Perímetro del triángulo: " << triangulo.calcularPerimetro() << std::endl;
+
+    Geometria::Rectangulo rectangulo(4, 6);
+    std::cout << "Área del rectángulo: " << rectangulo.calcularArea() << std::endl;
+    std::cout << "Perímetro del rectángulo: " << rectangulo.calcularPerimetro() << std::endl;
+
+    Geometria::Cuadrado cuadrado(4);
+    std::cout << "Área del cuadrado: " << cuadrado.calcularArea() << std::endl;
+    std::cout << "Perímetro del cuadrado: " << cuadrado.calcularPerimetro() << std::endl;
+
+    Geometria::Trapecio trapecio(3, 5, 4);
+    std::cout << "Área del trapecio: " << trapecio.calcularArea() << std::endl;
+    std::cout << "Perímetro del trapecio: " << trapecio.calcularPerimetro() << std::endl;
+
+    return 0;
+}
